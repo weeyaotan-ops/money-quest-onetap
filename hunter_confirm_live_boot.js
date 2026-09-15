7a74e83250ef0ac4946467dbb2f6d9acc3bf85e8
weeyaotan-ops <weeyaotan@gmail.com>
2026-09-15 10:57:32 +0800

    Boot Hunter confirm-live gateway with publisher

---
+++ hunter_confirm_live_boot.js
@@ -0,0 +1,9 @@
+'use strict';
+const {spawn}=require('node:child_process');
+function run(script,extra={},label=script){const p=spawn(process.execPath,[script],{env:{...process.env,...extra},stdio:['ignore','inherit','inherit']});p.on('exit',(c,s)=>{console.error(label+'_EXIT',c,s);if(label==='PUBLISHER')process.exit(c||1)});return p}
+const adapter=run('hunter_onetap_adapter.js',{HUNTER_ADAPTER_PORT:'18094'},'HUNTER_ADAPTER');
+const gateway=run('binance_onetap_gateway.js',{BINANCE_ONETAP_UPSTREAM:'http://127.0.0.1:18094/tickets'},'HUNTER_CONFIRM_LIVE');
+const publisher=run('resting_limit_loader_v2_candidate.js',{},'PUBLISHER');
+function stop(){for(const p of[adapter,gateway,publisher])try{p.kill('SIGTERM')}catch{}setTimeout(()=>process.exit(0),500).unref()}
+process.on('SIGTERM',stop);process.on('SIGINT',stop);
+console.log('HUNTER_CONFIRM_LIVE_BOOT',JSON.stringify({live:process.env.BINANCE_ONETAP_LIVE==='1',confirmationRequired:true,source:'Money Hunter'}));
